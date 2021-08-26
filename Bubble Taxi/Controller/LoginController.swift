9f87c96b44ba5102a91bca1c7271a04692f8be78
sanghee-dev <leeesangheee@gmail.com>
2021-08-26 23:05:40 +0900

    Add title label on the screen

--- Bubble Taxi/Controller/LoginController.swift
+++ Bubble Taxi/Controller/LoginController.swift
@@ -8,11 +8,28 @@
 import UIKit
 
 class LoginController: UIViewController {
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Bubble"
+        label.font = UIFont(name: "Futura", size: 36)
+        label.textColor = .systemTeal
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .cyan
+        view.backgroundColor = UIColor.init(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+        
+        view.addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,
+                                        constant: 40).isActive = true
     }
 
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .darkContent
+    }
 }
