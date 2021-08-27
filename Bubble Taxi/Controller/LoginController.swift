99ea9d7528cc8b370fba4835fa5afd62f16f03c3
sanghee-dev <leeesangheee@gmail.com>
2021-08-27 16:09:19 +0900

    Code refactor and build textFields on login screen

--- Bubble Taxi/Controller/LoginController.swift
+++ Bubble Taxi/Controller/LoginController.swift
@@ -18,51 +18,23 @@ class LoginController: UIViewController {
     }()
     
     private lazy var emailContainerView: UIView = {
-        let view = UIView()
-        
-        view.addSubview(emailIcon)
-        emailIcon.centerY(inView: view)
-        emailIcon.anchor(left: view.leftAnchor, paddingLeft: 8, width: 24)
-        
-        view.addSubview(emailTextField)
-        emailTextField.centerY(inView: view)
-        emailTextField.anchor(left: emailIcon.rightAnchor,
-                              bottom: view.bottomAnchor,
-                              right: view.rightAnchor,
-                              paddingLeft: 8,
-                              paddingBottom: 8)
-
-        view.addSubview(separatorView)
-        separatorView.anchor(left: emailTextField.leftAnchor,
-                             bottom: view.bottomAnchor,
-                             right: view.rightAnchor,
-                             height: 1)
-                
+        let view = UIView().textFieldContainerView(imageName: "envelope", textField: emailTextField)
+        view.heightAnchor.constraint(equalToConstant: 48).isActive = true
         return view
     }()
     
-    private let emailIcon: UIView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(systemName: "envelope")
-        imageView.alpha = 1
-        imageView.tintColor = .systemTeal
-        return imageView
+    private lazy var passwordContainerView: UIView = {
+        let view = UIView().textFieldContainerView(imageName: "lock", textField: passwordTextField)
+        view.heightAnchor.constraint(equalToConstant: 48).isActive = true
+        return view
     }()
     
     private let emailTextField: UITextField = {
-        let tf = UITextField()
-        tf.borderStyle = .none
-        tf.font = UIFont.systemFont(ofSize: 16)
-        tf.textColor = .black
-        tf.keyboardAppearance = .light
-        tf.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
-        return tf
+        return UITextField().textField(withPlaceholder: "Email", isSecureTextEntry: false)
     }()
     
-    private let separatorView: UIView = {
-        let separatorView = UIView()
-        separatorView.backgroundColor = .systemGroupedBackground
-        return separatorView
+    private let passwordTextField: UITextField = {
+        return UITextField().textField(withPlaceholder: "Password", isSecureTextEntry: true)
     }()
 
     override func viewDidLoad() {
@@ -71,19 +43,15 @@ class LoginController: UIViewController {
         view.backgroundColor = UIColor.init(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
         
         view.addSubview(titleLabel)
-        titleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor,
-                          left: view.leftAnchor,
-                          paddingTop: 60,
-                          paddingLeft: 32)
+        titleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, paddingTop: 60, paddingLeft: 32)
         
-        view.addSubview(emailContainerView)
-        emailContainerView.anchor(top: titleLabel.bottomAnchor,
-                                  left: view.leftAnchor,
-                                  right: view.rightAnchor,
-                                  paddingTop: 40,
-                                  paddingLeft: 32,
-                                  paddingRight: 32,
-                                  height: 50)
+        let stack = UIStackView(arrangedSubviews: [emailContainerView, passwordContainerView])
+        stack.axis = .vertical
+        stack.distribution = .fillEqually
+        stack.spacing = 16
+        
+        view.addSubview(stack)
+        stack.anchor(top: titleLabel.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 80, paddingLeft: 32, paddingRight: 32)
     }
 
     override var preferredStatusBarStyle: UIStatusBarStyle {
--- Bubble Taxi/Utils/Extensions.swift
+++ Bubble Taxi/Utils/Extensions.swift
@@ -9,6 +9,29 @@ import UIKit
 
 extension UIView {
     
+    func textFieldContainerView(imageName: String, textField: UITextField) -> UIView {
+        let view = UIView()
+        
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: imageName)
+        imageView.tintColor = .systemTeal
+    
+        view.addSubview(imageView)
+        imageView.centerY(inView: view)
+        imageView.anchor(left: view.leftAnchor, paddingLeft: 8, width: 24)
+        
+        view.addSubview(textField)
+        textField.centerY(inView: view)
+        textField.anchor(left: imageView.rightAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingLeft: 8, paddingBottom: 8)
+        
+        let separatorView = UIView()
+        separatorView.backgroundColor = .systemGroupedBackground
+        view.addSubview(separatorView)
+        separatorView.anchor(left: textField.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, height: 1)
+        
+        return view
+    }
+    
     func anchor(top: NSLayoutYAxisAnchor? = nil,
                 left: NSLayoutXAxisAnchor? = nil,
                 bottom: NSLayoutYAxisAnchor? = nil,
@@ -48,4 +71,19 @@ extension UIView {
     func centerY(inView view: UIView) {
         centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
+}
+
+extension UITextField {
+    
+    func textField(withPlaceholder placeholder: String, isSecureTextEntry: Bool) -> UITextField {
+        let tf = UITextField()
+        tf.borderStyle = .none
+        tf.font = UIFont.systemFont(ofSize: 16)
+        tf.textColor = .black
+        tf.keyboardAppearance = .light
+        tf.isSecureTextEntry = isSecureTextEntry
+        tf.attributedPlaceholder = NSAttributedString(string: placeholder, attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
+        return tf
+    }
+    
 }
